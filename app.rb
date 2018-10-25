53e49b9fb056f32e142694f3f30478b5e68645c0
Brandon Green <brandon@brandonleeproductions.com>
2018-10-25 23:32:00 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -54,8 +54,8 @@ class App < Sinatra::Base
 
   get '/logout' do
     #session.clear works too
-    #session = {} is what .clear method does
-    session[:id].clear
+    session = {} #is what .clear method does
+    #session[:id].clear
     "Session has now been cleared. session content: #{session.inspect}. Continue on to the '/finish' line!"
   end
 
